ecf25497e60694bb2d35fb34c33f9e3776ae436d
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-12-01 16:23:55 +0530

    Added: displaying message using requestparam passing key and value

--- src/main/java/com/bridgelabz/firstspringapp/controller/HelloWorldController.java
+++ src/main/java/com/bridgelabz/firstspringapp/controller/HelloWorldController.java
@@ -1,14 +1,22 @@
 package com.bridgelabz.firstspringapp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
+//@RequestMapping("/hello")
 public class HelloWorldController {
     @RequestMapping(value = "/home")
     public String sayHello() {
         return "Hello from Bridgelabz";
     }
 
+    @GetMapping(value = "/query")
+    public String sayHello(
+            @RequestParam(value = "name") String Sunil
+    ) {
+        return "Hello" + Sunil + "!";
+    }
 }
