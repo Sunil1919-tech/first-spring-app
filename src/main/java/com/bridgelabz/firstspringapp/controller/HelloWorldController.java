3498c8941a086f59f388effcc5e1979c8166fb2b
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-12-01 17:09:01 +0530

    Added: Performed http postMethod using requestBody

--- src/main/java/com/bridgelabz/firstspringapp/controller/HelloWorldController.java
+++ src/main/java/com/bridgelabz/firstspringapp/controller/HelloWorldController.java
@@ -23,4 +23,11 @@ public class HelloWorldController {
     ) {
         return "Hello" + name + "----!";
     }
+
+    @PostMapping("/post")
+    public String sayHello(
+            @RequestBody User user
+    ) {
+        return "Hello" + user.getFirstName() + " " + user.getLastName() + " " + user.getId();
+    }
 }
---
+++ src/main/java/com/bridgelabz/firstspringapp/controller/User.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.firstspringapp.controller;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private int id;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
