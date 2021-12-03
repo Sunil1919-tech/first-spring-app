f392c86f35355669268d2b6ee13f8e81a467f617
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-12-03 15:04:01 +0530

    Added: using slf4j package to log the messages

--- src/main/java/com/bridgelabz/firstspringapp/FirstSpringAppApplication.java
+++ src/main/java/com/bridgelabz/firstspringapp/FirstSpringAppApplication.java
@@ -1,14 +1,23 @@
 package com.bridgelabz.firstspringapp;
 
+import com.bridgelabz.firstspringapp.components.DemoBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 
 @SpringBootApplication
 public class FirstSpringAppApplication {
+    public static final Logger logger = LoggerFactory.getLogger(FirstSpringAppApplication.class);
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to My first Spring Application");
-		SpringApplication.run(FirstSpringAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        System.out.println("Welcome to My first Spring Application");
+        ApplicationContext context = SpringApplication.run(FirstSpringAppApplication.class, args);
+        logger.debug("Checking Context:" + context.getBean(DemoBean.class));
+        logger.debug("\n**** Example using @autoWire annotation on property ***");
+//		SpringApplication.run(FirstSpringAppApplication.class);
+        logger.debug("Hello from Bridgelabz");
+    }
 }
---
+++ src/main/java/com/bridgelabz/firstspringapp/components/DemoBean.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.firstspringapp.components;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoBean {
+   public DemoBean(){
+
+   }
+}
